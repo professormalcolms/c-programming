35201f853a73d19d9a74fc7bea0113191a2f073f
PROFESSOR MALCOLM <accounting@professormalcolms.com>
2016-08-24 12:03:22 -0400

    Commit HelloWorld

---
+++ HelloWorld/main.c
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: mathtutor
+ *
+ * Created on August 23, 2016, 3:05 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int j=1;
+    for (int i=0; i<10; i++)
+    {
+        j = j+1;
+        /*printf("%d\n", i);*/
+    }
+    printf("Hello, world\n");
+}
+
